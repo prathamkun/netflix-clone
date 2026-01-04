57b198379cb20b2b9ce2ebc5dc39208bd8f68319
prathameshborkar <prathamkun@users.noreply.github.com>
2026-01-04 22:49:38 +0530

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="navbar-left">
+        <img src={logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+      </div>
+      <div className="navbar-right"></div>
+    </div>
+  )
+}
+
+export default Navbar
