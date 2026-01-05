32c822eb5de9c00f97f39bf6f0b675e817ca2430
prathameshborkar <prathamkun@users.noreply.github.com>
2026-01-06 00:05:29 +0530

    databse

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -10,6 +10,14 @@ const Login = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const user_auth = async (event)=>{
+    event.preventDefault();
+    if(signState==="Sign In"){
+      await login(email,password);
+    }else{
+      await signup(name,email,password);
+    }
+  }
 
 
 
@@ -25,10 +33,10 @@ const Login = () => {
         <h1>{signState}</h1>
         <form >
           {signState==="Sign Up"?
-          <input value={name} onChange={(e)=>{e.target.value}} type="text" placeholder='Your name' />:<></>}
-          <input value={email} onChange={(e)=>{e.target.value}} type="email" placeholder='Email'/>
-          <input value={password} onChange={(e)=>{e.target.value}} type="password" placeholder='Password'/>
-          <button>{signState}</button>
+          <input value={name} onChange={(e)=>{setName(e.target.value)}} type="text" placeholder='Your name' />:<></>}
+          <input value={email} onChange={(e)=>{setEmail(e.target.value)}} type="email" placeholder='Email'/>
+          <input value={password} onChange={(e)=>{setPassword(e.target.value)}} type="password" placeholder='Password'/>
+          <button onClick={user_auth} type="submit">{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
