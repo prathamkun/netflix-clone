b769f6bb39ce32595f8f8e8d81a7279b40d124ec
prathameshborkar <prathamkun@users.noreply.github.com>
2026-01-06 23:03:32 +0530

    icon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import './Footer.css'
 import youtube_icon from '../../assets/youtube_icon.png'
-import twitter_icon from '../../assets/twitter_icon.png'
+import twitter_icon from '../../assets/twitter.png'
 import instagram_icon from '../../assets/instagram_icon.png'
 import facebook_icon from '../../assets/facebook_icon.png'
 
-
 const Footer = () => {
   return (
-    <div className='footer'>
+    <div className="footer">
       <div className="footer-icons">
-        <img src={facebook_icon} alt="" />
-        <img src={instagram_icon} alt="" />
-        <img src={twitter_icon} alt="" />
-        <img src={youtube_icon} alt="" />
+        <img src={facebook_icon} alt="Facebook" />
+
+        <a
+          href="https://www.instagram.com/pratham.huh/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={instagram_icon} alt="Instagram" />
+        </a>
+
+        <a
+          href="https://x.com/prathamiscoding"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={twitter_icon} alt="Twitter" />
+        </a>
+
+        <img src={youtube_icon} alt="YouTube" />
       </div>
+
       <ul>
         <li>Audio Description</li>
         <li>Help Center</li>
@@ -29,7 +44,10 @@ const Footer = () => {
         <li>Corporate Information</li>
         <li>Contact Us</li>
       </ul>
-      <p className='copyright-text'>@ 1997-2026 Prathamflix, Inc.</p>
+
+      <p className="copyright-text">
+        © 1997–2026 Prathamflix, Inc.
+      </p>
     </div>
   )
 }
