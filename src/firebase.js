ff13b41a037afd5f6ab9bd372168b7470c27548f
prathameshborkar <prathamkun@users.noreply.github.com>
2026-01-06 12:19:09 +0530

    media query and css properties

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,12 @@
-
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth/cordova";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut
+} from "firebase/auth";
 import { addDoc, collection, getFirestore } from "firebase/firestore/lite";
-
+import { toast } from "react-toastify";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCgCd2br2I8ztEMiHb3jTywjgo90MifmU8",
@@ -13,42 +17,42 @@ const firebaseConfig = {
   appId: "1:37948290348:web:371bcc8a7a5cc3b008e889"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const signup = async (name, email, password)=>{
-    try {
-        const res = await createUserWithEmailAndPassword(auth, email, password);
-        const user = res.user;
-        await addDoc(collection(db,"user"), {
-            uid: user.uid,
-            name,
-            authProvider: "local",
-            email,
-        });
-        
-    } catch (error){
-        console.log(error);
-        alert(error);
+const signup = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
 
-    }
+    await addDoc(collection(db, "user"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email
+    });
 
-}
+    toast.success("Account created successfully 🎉");
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].replaceAll("-", " "));
+  }
+};
 
-const login = async (email, password)=>{
-    try {
-        signInWithEmailAndPassword(auth, email, password);
-        
-    } catch (error) {
-       console.log(error);
-       alert(error); 
-    }
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    toast.success("Logged in successfully 🚀");
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].replaceAll("-", " "));
+  }
+};
 
-}
-const logout = ()=>{
-    signOut(auth);
-}
+const logout = async () => {
+  await signOut(auth);
+  toast.info("Logged out");
+};
 
-export {auth, db, login, signup, logout};
+export { auth, db, login, signup, logout };
